e8c6e98993721f3c79d97558e0180a19da54c73c
abu said <mdabusaidriyaz@gmail.com>
2021-05-04 10:39:04 +0600

    add operator file

---
+++ operator.py
@@ -0,0 +1,32 @@
+num1 = int(input("Enter your first number :"))
+num2 = int(input("Enter your second number :"))
+
+# sumation result of both number
+
+sum = num1 + num2
+print("your summation result is", sum)
+
+# substraction result
+
+subsum = num1 - num2
+print("your substraction result is", subsum)
+
+# multiplication result of both number
+
+mul = num1 * num2
+print("your multiplication result is", mul)
+
+# division result
+
+div = num1 / num2
+print("your division result is", div)
+
+# floor result
+
+floor = num1 // num2
+print("your floor result is", floor)
+
+# remainder result
+
+rem = num1 % num2
+print("your remainder result is", rem)
